14d2245f664f1136dc11bcc59abc9d13a7dfb8ce
Goutham Kumar <gouthamcumar@gmail.com>
2016-01-07 10:42:26 +0530

    Adding discovery cycle

--- index.py
+++ index.py
@@ -18,17 +18,45 @@ class CollectionThread(threading.Thread):
     def __init__(self, vcenter):
         threading.Thread.__init__(self)
         self.vcenter = vcenter
+        self._lock = threading.Lock()
 
     def run(self):
-        vmware = VMWare(self.vcenter)
-        vmware.discovery()
+        self.vmware = VMWare(self.vcenter)
+
+        self.discovery_thread = threading.Thread(target=self._discovery)
+        self.discovery_thread.setName(self.vcenter['host'] + "_" + "Discovery")
+        self.discovery_thread.start()
+
         while True:
             try:
-                vmware.collect()
+                self._lock.acquire()
+                util.sendEvent("Collection", "Collection Lock acquired", "info")
+                self.vmware.collect()
+                self._lock.release()
+                util.sendEvent("Collection", "Collection Lock released", "info")
+
                 time.sleep(float(self.vcenter.get("pollInterval", 1000) / 1000))
             except StandardError as se:
-                util.sendEvent("Unknown Error", "Unknown error occurred: [" +str(se) + "]", "critical")
+                util.sendEvent("Unknown Error", "Unknown error occurred: [" + str(se) + "]", "critical")
+                if self._lock.locked:
+                    self._lock.release
                 sys.exit(-1)
+
+    def _discovery(self):
+        while True:
+            try:
+                self._lock.acquire()
+                util.sendEvent("Discovery", "Discovery Locke acquired", "info")
+                self.vmware.discovery()
+                self._lock.release()
+                util.sendEvent("Discovery", "Discovery Locke released", "info")
+
+                time.sleep(self.vcenter.get("discoveryInterval", 10800))
+            except StandardError as se:
+                util.sendEvent("Unknown Error", "Unknown error occurred: [" + str(se) + "]", "error")
+                if self._lock.locked:
+                    self._lock.release
+                return
 
 if __name__ == "__main__":
     params = util.parse_params()
@@ -38,6 +66,3 @@ if __name__ == "__main__":
         thread = CollectionThread(vcenter)
         thread.setName(vcenter['host'])
         thread.start()
-
-    while(True):
-        pass

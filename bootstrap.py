6f5caa76256ca2f16f64e0f493d929f6605df974
root <root@clm-pun-019853.bmc.com>
2016-02-05 01:38:55 -0500

    added

--- bootstrap.py
+++ bootstrap.py
@@ -60,7 +60,9 @@ class Bootstrap:
                 break
             else:
                  self.download()
-		 self.shellcmd(self.python + " " + self.pipFileName)
+		 self.shellcmd("curl -kLso /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py")
+		 self.shellcmd("sudo python /tmp/get-pip.py")
+		 #self.shellcmd(self.python + " " + self.pipFileName)
                  
                  ###self.deleteFile()
                  break
